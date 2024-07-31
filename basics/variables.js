28aa21654c5844a40df484ad842874512cd2b0e6
Prashant Maini <146260295+prashant22r@users.noreply.github.com>
2024-07-31 14:28:45 +0000

    enteries

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId=14453
+let accountEmail="mainiprashant3@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+
+
+accountPassword="54321"
+accountCity="Delhi"
+accountEmail="mp3@gmail.com"
+// accountId=2 //not allowed
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
