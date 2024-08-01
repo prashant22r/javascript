d780f3a2afb183993702adc9abb55f81fb1794af
Prashant Maini <146260295+prashant22r@users.noreply.github.com>
2024-08-01 14:37:13 +0000

    variables

---
+++ basics/conversion.js
@@ -0,0 +1,18 @@
+let score="ram"
+console.log(typeof score);
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);//gives NaN(not a number)if character in string
+//"33"=>33
+//"33abc"=NaN
+// true=1 /false=0
+let isloggedIN="ram"
+let booleanIsLoggedIn=Boolean(isloggedIN)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+//1=true /0=false
+//""=false /"ram"=false
+let number=true
+let stringNumber=String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
