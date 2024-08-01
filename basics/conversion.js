6cd31d1e331613dba862789219e23301f8dc4ff9
Prashant Maini <146260295+prashant22r@users.noreply.github.com>
2024-08-01 15:11:01 +0000

    operations

--- basics/conversion.js
+++ basics/conversion.js
@@ -1,18 +1,33 @@
-let score="ram"
-console.log(typeof score);
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);//gives NaN(not a number)if character in string
-//"33"=>33
-//"33abc"=NaN
-// true=1 /false=0
-let isloggedIN="ram"
-let booleanIsLoggedIn=Boolean(isloggedIN)
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
-//1=true /0=false
-//""=false /"ram"=false
-let number=true
-let stringNumber=String(number)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let score="ram"
+// console.log(typeof score);
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);//gives NaN(not a number)if character in string
+// //"33"=>33
+// //"33abc"=NaN
+// // true=1 /false=0
+// let isloggedIN="ram"
+// let booleanIsLoggedIn=Boolean(isloggedIN)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+// //1=true /0=false
+// //""=false /"ram"=false
+// let number=true
+// let stringNumber=String(number)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//**************operations***************/
+// let value=3
+// let negValue= -value
+// console.log(negValue);
+let str1="hello"
+let str2=" prashant"
+let str3=str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+"2");
+console.log(1+2+"2");
+console.log(+true);
+console.log(+"");
