51ac4c1e907cb5aa80ea32c775dce27c5b33b598
Prashant Maini <146260295+prashant22r@users.noreply.github.com>
2024-08-26 15:00:56 +0000

    inbuilt

---
+++ basics/inbuiltfn.js
@@ -0,0 +1,30 @@
+let Name="Prashant Maini";
+function getlength(str){
+    console.log("original string : " , str);
+    console.log("string length : ", str.length);
+}
+getlength(Name);
+
+function getIndexOf(str,target){
+    console.log("Index : ",str.indexOf(target));
+}
+getIndexOf(Name,"n");
+function getLastIndexOf(str,target){
+    console.log("Index : ",str.lastIndexOf(target));
+}
+getLastIndexOf(Name,"n");
+//slice
+function getSlice(str ,start ,end){
+    console.log("After slice : ",str.slice(start,end));
+}
+getSlice(Name,2,5);
+//substring
+function getSubString(str,start ,end) {
+    console.log("new substring : ",str.substr(start,end));
+}
+getSubString(Name,2,5);
+//replace 
+function replaceString(str,target,replacement){
+    console.log("Replaced string : ",str.replace(target,replacement));
+}
+replaceString(Name,"Maini","Mishra");
